5117caa2c873085077374a3d889f6696d7be8e30
MD. Zakirul Islam <rifat40520@gmail.com>
2025-09-13 23:26:52 +0600

    13.925

---
+++ CarPartsProject/Manager/View/Dashboard.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Manager Dashboard</title>
+        <link rel="Styledheet" href="Dashboard_style.css">
+    </head>
+</html>
+<body>
+    <div class="dashboard">
+        <h1>Manager Dashboard</h1>
+        <p>Welcome, Please select an action:</p>
+        <div class="button-grid">
+            <form action="Approve_orders.php" method="POST">
+                <button type="submit" class="btn">Approve/Reject purchase Order</button>
+            </form>
+            <form action="Supplier_report.php" method="POST">
+                <button type="submit" class="btn">Generate Suplier Performance Report</button>
+            </form>
+            <form action="Set_record.php" method="POST">
+                <button type="submit" class="btn">Set Parts Reorder Levels</button>
+            </form>
+            <form action="Adjust_price.php" method="POST">
+                <button type="submit" class="btn">Adjust Parts Sale Price</button>
+            </form>
+            <form action="Authorize_returns.php" method="POST">
+                <button type="submit" class="btn">Authorize Customer Returns</button>
+            </form>
+            <form action="Summary_report.php" method="POST">
+                <button type="submit" class="btn">Generate Summary Reports</button>
+            </form>
+            <form action="Stock_categories.php" method="POST">
+                <button type="submit" class="btn">Manage Stock Categories</button>
+            </form>
+
+        </div>
+    </div>
+</body>
